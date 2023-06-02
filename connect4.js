d592ae5cc07034e7e99945a5d172c4d25da99f4c
Hossein-K-R <116842837+Hossein-K-R@users.noreply.github.com>
2023-06-02 11:32:07 +0200

    Connect 4

---
+++ connect4.js
@@ -0,0 +1,178 @@
+const person1Name = localStorage.getItem('person1Name');
+const person2Name = localStorage.getItem('person2Name');
+console.log(person1Name, person2Name)
+var playerRed = person1Name;
+var playerYellow = person2Name;
+var currPlayer = playerRed;
+
+var gameOver = false;
+var board;
+var currColumns;
+
+var rows = 6;
+var columns = 7;
+
+
+const form = document.getElementById('form');
+const submitButton = document.getElementById('submitButton');
+
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const person1Name = document.getElementById('person1').value;
+  const person2Name = document.getElementById('person2').value;
+
+  if (person1Name && person2Name) {
+    localStorage.setItem('person1Name', person1Name);
+    localStorage.setItem('person2Name', person2Name);
+    
+    location.reload();
+  }
+});
+
+window.addEventListener('DOMContentLoaded', function() {
+  const person1Name = localStorage.getItem('person1Name');
+  const person2Name = localStorage.getItem('person2Name');
+
+  if (person1Name && person2Name) {
+
+    const namesElement = document.createElement('h4');
+    const node = document.createTextNode("This is a paragraph.");
+    namesElement.textContent = `Player1: ${person1Name} and Player2: ${person2Name}`;
+    document.getElementById('names').appendChild(namesElement);
+  } else {
+    form.style.display = 'block';
+  }
+});
+
+
+
+
+window.onload = function () {
+  setGame();
+};
+
+function setGame() {
+  board = [];
+  currColumns = [5, 5, 5, 5, 5, 5, 5];
+  for (let r = 0; r < rows; r++) {
+    let row = [];
+    for (let c = 0; c < columns; c++) {
+      row.push(" ");
+      let tile = document.createElement("div");
+      tile.id = r.toString() + "-" + c.toString();
+      tile.classList.add("tile");
+      tile.addEventListener("click", setPiece);
+      document.getElementById("board").append(tile);
+    }
+    board.push(row);
+  }
+}
+
+function setPiece() {
+  if (gameOver) {
+    return;
+  }
+
+  let coords = this.id.split("-");
+  let r = parseInt(coords[0]);
+  let c = parseInt(coords[1]);
+
+  r = currColumns[c];
+  if (r < 0) {
+    return;
+  }
+
+  board[r][c] = currPlayer;
+  let tile = document.getElementById(r.toString() + "-" + c.toString());
+  if (currPlayer == playerRed) {
+    tile.classList.add("red-piece");
+    currPlayer = playerYellow;
+  } else {
+    tile.classList.add("yellow-piece");
+    currPlayer = playerRed;
+  }
+  r -= 1;
+  currColumns[c] = r;
+
+  checkWinner();
+}
+
+function checkWinner() {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r][c + 1] &&
+          board[r][c + 1] == board[r][c + 2] &&
+          board[r][c + 2] == board[r][c + 3]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+
+  for (let c = 0; c < columns; c++) {
+    for (let r = 0; r < rows - 3; r++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r + 1][c] &&
+          board[r + 1][c] == board[r + 2][c] &&
+          board[r + 2][c] == board[r + 3][c]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+
+  for (let r = 0; r < rows - 3; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r + 1][c + 1] &&
+          board[r + 1][c + 1] == board[r + 2][c + 2] &&
+          board[r + 2][c + 2] == board[r + 3][c + 3]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+
+  for (let r = 3; r < rows; r++) {
+    for (let c = 0; c < columns - 3; c++) {
+      if (board[r][c] != " ") {
+        if (
+          board[r][c] == board[r - 1][c + 1] &&
+          board[r - 1][c + 1] == board[r - 2][c + 2] &&
+          board[r - 2][c + 2] == board[r - 3][c + 3]
+        ) {
+          setWinner(r, c);
+          return;
+        }
+      }
+    }
+  }
+}
+
+function setWinner(r, c) {
+  let winner = document.getElementById("winner");
+  if (board[r][c] == playerRed) {
+    winner.innerText =   `${playerRed} wins!`;
+  } else {
+    winner.innerText = `${playerYellow} wins!`;
+  }
+  gameOver = true;
+}
+
+
+const refreshButton = document.getElementById('refreshButton');
+
+refreshButton.addEventListener('click', function() {
+    location.reload();
+});
